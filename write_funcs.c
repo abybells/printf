f80406f1b074d9039027ffd5c8cd27f2c596fd1d
abybells <abbiolbello@yahoo.com>
2022-04-19 08:09:35 -0700

    write_func.c file added

---
+++ write_funcs.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the char c to stdout
+ * @c: the char to print
+ * Return: On succes 1.
+ * On error, -1 is returned, and errno is set appropiately.
+ * Description: _putchar uses a local buffer of 1024 to call write as little as possible
+ */
+int _putchar(char c)
+{
+	static char buf[1024];
+	static int i;
+
+	if (c == -1 || i >= 1024)
+	{
+		write(1, &buf, i);
+		i = 0;
+	}
+	if (c != -1)
+	{
+		buf[i] = c;
+		i++;
+	}
+	return (1);
+}
