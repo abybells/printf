6dad89549e5e521ea3e904baa3fae51ba19b3c10
abybells <abbiolbello@yahoo.com>
2022-04-19 11:07:14 -0700

    print_alpha.c file updated

--- print_alpha.c
+++ print_alpha.c
@@ -19,14 +19,14 @@ int print_string(va_list l, flags_t *f)
 
 /**
  * print_char - prints a character
- * @1: va_list arguments from _printf
+ * @l: va_list arguments from _printf
  * if a flag is passed to _printf
  * Return: number of char printed
  */
-int print_char(va_list 1, flags_t *f)
+int print_char(va_list l, flags_t *f)
 {
 	(void)f;
-	_putchar(va_arg(1, int));
+	_putchar(va_arg(l, int));
 	return (1);
 }
 
