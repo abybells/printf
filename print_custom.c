d4eea5f137b39d1acaf4d97131d914b5d25c559b
gharnie <bamigbopaganiyu1@gmail.com>
2022-04-19 18:28:33 +0100

    print_custom.c file added

--- print_custom.c
+++ print_custom.c
@@ -58,3 +58,17 @@ int print_rev(va_list l, flags_t *f)
 
 	return (i);
 }
+
+/**
+ * print_percent - prints a percent
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags in which we turn the flags on
+ * Return: number of char printed
+ */
+int print_percent(va_list l, flags_t *f)
+{
+	(void)l;
+	(void)f;
+	return (_putchar('%'));
+}
+
