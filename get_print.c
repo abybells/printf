9ad628ffe1c94c53723f4526f10fbbd25d45673c
gharnie <bamigbopaganiyu1@gmail.com>
2022-04-19 18:26:37 +0100

    get_print.c file added

--- get_print.c
+++ get_print.c
@@ -30,3 +30,10 @@ int (*get_print(char s))(va_list, flags_t *)
 	int flags = 14;
 
 	register int i;
+
+	for (i =0; i < flags; i++)
+		if (func_arr[i].c == s)
+			return (func_arr[i].f);
+	return (NULL);
+}
+
